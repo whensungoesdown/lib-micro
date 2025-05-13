8076717c8c6ae5b07a37273a285ed78c753df6d6
uty <whensungoes@gmail.com>
2025-05-13 07:38:28 -0400

    this does not froze the system, an empty hook
    Because it takes less space in MSRAM, so it does not overwrite
    other patch's code.

--- tools/cmps/cmps.c
+++ tools/cmps/cmps.c
@@ -19,10 +19,13 @@ void install_jump_target(void) {
 
     ucode_t ucode_patch[] = {
     //{ MOVE_DSZ64_DI(RAX, 0x1234), MOVE_DSZ64_DR(RBX, TMP1), NOP,
-    //        SEQ_NEXT | SEQ_SYNCWTMRK(2) }, //0x7d00
-    { MOVE_DSZ64_DI(RAX, 0x1234), NOP, NOP,
-            END_SEQWORD }, //0x7d00
-        {UNK256, NOP, NOP, END_SEQWORD}, //0x7d04
+//    { MOVE_DSZ64_DI(RAX, 0x1234), NOP, NOP,
+//            SEQ_NEXT | SEQ_SYNCWTMRK(2) }, //0x7d00
+//    { MOVE_DSZ64_DI(RAX, 0x1234), NOP, NOP,
+//    { NOP, NOP, NOP,
+//            END_SEQWORD }, //0x7d00
+    {MOVE_DSZ64_DI(RAX, 0x1234), NOP, NOP, 0x018000f0} // SEQW UEND0
+//        {UNK256, NOP, NOP, END_SEQWORD}, //0x7d04
     };
 //    if (verbose)
         printf("patching addr: %08lx - ram: %08lx\n", addr, ucode_addr_to_patch_addr(addr));
@@ -37,7 +40,7 @@ void hook_cmps(u64 addr, u64 hook_address, u64 idx) {
         printf("persistent_trace only supports 4-aligned uaddrs currently. (%04lx)\n", hook_address);
     return;
     }
-    install_jump_target();
+    //install_jump_target();
     u64 uop0 = ldat_array_read(0x6a0, 0, 0, 0, hook_address+0) & CRC_UOP_MASK;
     u64 uop1 = ldat_array_read(0x6a0, 0, 0, 0, hook_address+1) & CRC_UOP_MASK;
     u64 uop2 = ldat_array_read(0x6a0, 0, 0, 0, hook_address+2) & CRC_UOP_MASK;
@@ -49,65 +52,80 @@ void hook_cmps(u64 addr, u64 hook_address, u64 idx) {
 
     ucode_t ucode_patch[] = {
         {   // 0x0
-            UJMP_I(addr+0x4),
-            UJMP_I(addr+0x15),
-            UJMP_I(addr+0x16),
-            NOP_SEQWORD
-        },
-        {   // 0x4
-            //TESTUSTATE_SYS_NOT(0x2),
-            //STADSTGBUF_DSZ64_ASZ16_SC1_RI(TMP0, 0xba40),
-            //STADSTGBUF_DSZ64_ASZ16_SC1_RI(TMP1, 0xba80),
-            //SEQ_GOTO0(addr+0x14)
-
-            LDZX_DSZ64_ASZ32_SC1_DR(TMP0, RDI, 0x18),  // dst_reg, src_reg, seg
-            ZEROEXT_DSZ32_DI(TMP1, 0x4141),
-            SUBR_DSZ64_DRR(TMP0, TMP0, TMP1),   // dst, src0, src1
-	    //NOP,
-	    //NOP,
-	    //NOP,
-            NOP_SEQWORD
-
-        },
-        {   // 0x8
-            //ZEROEXT_DSZ32_DI(TMP0, 0xdead),
-            //CONCAT_DSZ16_DRR(TMP0, TMP0, TMP0),
-            //CONCAT_DSZ32_DRR(TMP0, TMP0, TMP0),
+            //UJMP_I(addr+0x4),
+            //UJMP_I(addr+0x15),
+            //UJMP_I(addr+0x16),
             //NOP_SEQWORD
-            
-	    UJMPCC_DIRECT_NOTTAKEN_CONDZ_RI(TMP0, JUMP_DESTINATION),
-	    //MOVE_DSZ64_DI(RAX, 0xabcd),
-        //    NOP,
-            NOP,
-            NOP,
+
+            UJMP_I(addr+0x4),
+            UJMP_I(addr+0x5),
+            UJMP_I(addr+0x6),
             NOP_SEQWORD
-	    //END_SEQWORD
-        },
-        {   // 0xc
-            //XOR_DSZ64_DRR(TMP0, TMP0, RAX),
-            //MOVE_DSZ64_DI(TMP1, hook_address),
-            //UJMPCC_DIRECT_NOTTAKEN_CONDZ_RI(TMP0, JUMP_DESTINATION),
-            //NOP_SEQWORD,
-            NOP,
-            NOP,
-            NOP,
-            NOP_SEQWORD
-        },
-        {   // 0x10
-            //LDSTGBUF_DSZ64_ASZ16_SC1_DI(TMP0, 0xba40),
-            //LDSTGBUF_DSZ64_ASZ16_SC1_DI(TMP1, 0xba80),
+
             //NOP,
-            //NOP_SEQWORD,
-            NOP,
-            NOP,
-            NOP,
-            NOP_SEQWORD
+            //NOP,
+            //NOP,
+            //SEQ_GOTO0(addr+0x14)
         },
-        {   // 0x14
+//        {   // 0x4
+//            //TESTUSTATE_SYS_NOT(0x2),
+//            //STADSTGBUF_DSZ64_ASZ16_SC1_RI(TMP0, 0xba40),
+//            //STADSTGBUF_DSZ64_ASZ16_SC1_RI(TMP1, 0xba80),
+//            //SEQ_GOTO0(addr+0x14)
+//
+////            LDZX_DSZ64_ASZ32_SC1_DR(TMP0, RDI, 0x18),  // dst_reg, src_reg, seg
+////            ZEROEXT_DSZ64_DI(TMP1, 0x4142),
+////            //SUBR_DSZ64_DRR(TMP0, TMP0, TMP1),   // dst, src0, src1
+////            XOR_DSZ64_DRR(TMP0, TMP0, TMP1),
+//	    NOP,
+//	    NOP,
+//	    NOP,
+//            NOP_SEQWORD
+//
+//        },
+//        {   // 0x8
+//            //ZEROEXT_DSZ32_DI(TMP0, 0xdead),
+//            //CONCAT_DSZ16_DRR(TMP0, TMP0, TMP0),
+//            //CONCAT_DSZ32_DRR(TMP0, TMP0, TMP0),
+//            //NOP_SEQWORD
+//            
+////	    UJMPCC_DIRECT_NOTTAKEN_CONDZ_RI(TMP0, JUMP_DESTINATION),
+//	    //MOVE_DSZ64_DI(RAX, 0xabcd),
+//            NOP,
+//            NOP,
+//            NOP,
+//            NOP_SEQWORD
+//	    //END_SEQWORD
+//        },
+//        {   // 0xc
+//            //XOR_DSZ64_DRR(TMP0, TMP0, RAX),
+//            //MOVE_DSZ64_DI(TMP1, hook_address),
+//            //UJMPCC_DIRECT_NOTTAKEN_CONDZ_RI(TMP0, JUMP_DESTINATION),
+//            //NOP_SEQWORD,
+//            NOP,
+//            NOP,
+//            NOP,
+//            NOP_SEQWORD
+//        },
+//        {   // 0x10
+//            //LDSTGBUF_DSZ64_ASZ16_SC1_DI(TMP0, 0xba40),
+//            //LDSTGBUF_DSZ64_ASZ16_SC1_DI(TMP1, 0xba80),
+//            //NOP,
+//            //NOP_SEQWORD,
+//            NOP,
+//            NOP,
+//            NOP,
+//            NOP_SEQWORD
+//        },
+        {   // 0x14  0x4
             uop0, uop1, uop2, seqw
         },
         {   // 0x18
             UJMP_I(hook_address+4), UJMP_I(hook_address+5), UJMP_I(hook_address+6), NOP_SEQWORD
+            //NOP,
+            //NOP,
+            //NOP,
+            //SEQ_GOTO0(0x08b4)
         }
     };
 
@@ -197,12 +215,76 @@ int list_match_patch()
     return 0;
 }
 
+struct _seqw
+{
+    unsigned int up0:2;
+    unsigned int eflow:4;
+    unsigned int up1:2;
+    unsigned int uaddr:15;
+    unsigned int up2:2;
+    unsigned int sync:3;
+    unsigned int crc:2;
+    unsigned int reserved:2;
+};
+
+void print_seqw (int value)
+{
+    struct _seqw* p = 0;
+
+    p = (struct _seqw*)&value;
+
+    printf("up0  : 0x%x\n", p->up0);
+    printf("eflow: 0x%x\n", p->eflow);
+    printf("up1  : 0x%x\n", p->up1);
+    printf("uaddr: 0x%x\n", p->uaddr);
+    printf("up2  : 0x%x\n", p->up2);
+    printf("sync : 0x%x\n", p->sync);
+    printf("crc  : 0x%x\n", p->crc);
+
+    return;
+}
+
 int main(int argc, char* argv[]) {
 
 
     int i = 0;
 
     assign_to_core(0);
+
+//    // test
+//    printf("SEQ_UEND0(0) 0x%x\n", SEQ_UEND0(0));
+//    printf("SEQ_UEND0(1) 0x%x\n", SEQ_UEND0(1));
+//    printf("SEQ_UEND0(2) 0x%x\n", SEQ_UEND0(2));
+//    printf("SEQ_UEND0(3) 0x%x\n", SEQ_UEND0(3));
+//
+//    printf("END_SEQWORD 0x%x\n", END_SEQWORD);
+//
+
+//    printf("0x018000f2\n");
+//    print_seqw(0x018000f2);
+//    printf("\n");
+//
+//    printf("0x018000c0\n");
+//    print_seqw(0x018000c0);
+//    printf("\n");
+//
+//    printf("NOP_SEQWORD\n");
+//    print_seqw(NOP_SEQWORD);
+//    printf("\n");
+//
+//    printf("END_SEQWORD\n");
+//    print_seqw(END_SEQWORD);
+//    printf("\n");
+//
+//    printf("0x80000c0\n");
+//    print_seqw(0x8000c0);
+//    printf("\n");
+//
+//    printf("0x18b9100\n");
+//    print_seqw(0x18b9100);
+//    printf("\n");
+//
+//    return 0;
 
 
 //    do_fix_IN_patch();
@@ -216,7 +298,7 @@ int main(int argc, char* argv[]) {
 
 #define CMPS_XLAT   0x08b0
 
-    hook_cmps(0x7c20, CMPS_XLAT, 1);
+    hook_cmps(0x7c10, CMPS_XLAT, 1);
 
     //    printf("press any key to run test().\n");
     //    getchar();
